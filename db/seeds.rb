03b5d921722291be5f1e7411089f7cb46a95effb
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-09 17:04:26 +0100

    Exercise with articles begun

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::BigBangTheory.character,
+    content: Faker::TvShows::BigBangTheory.quote
+  )
+  article.save!
+end
+puts 'Finished!'
